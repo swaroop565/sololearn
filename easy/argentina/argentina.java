934bc59c6ab5758ddf67d11d0bd0886c1f1c8d15
swaroop565 <83798573+swaroop565@users.noreply.github.com>
2021-05-07 08:29:02 +0530

    Create argentina.java

---
+++ easy/argentina/argentina.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public final class Argentina {
+    public static void main(final String[] args) {
+        final var scanner = new Scanner(System.in);
+        final var pesos = scanner.nextInt();
+        final var dollars = scanner.nextInt();      
+        System.out.println(
+            dollars * 50l > pesos ? "Pesos" : "Dollars"
+        );
+    }
+}
